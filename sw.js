00e30a70eca8ea4cc5f0b364fed0c25606f570f7
AeroVista <aerovistaus@gmail.com>
2026-09-04 01:58:27 -0700

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'echoverse-phase-shell-v1';
+const CORE = ['/', '/index.html', '/manifest.webmanifest', '/icons/phase.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+      if (response && response.ok && new URL(event.request.url).origin === self.location.origin) {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => caches.match('/index.html')))
+  );
+});
