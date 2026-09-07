46392f788344ee4bd247431398623ccff3821eb9
AeroVista <aerovistaus@gmail.com>
2026-09-06 20:43:34 -0700

    Report Phase sparse render stats from worker

--- js/render-worker.js
+++ js/render-worker.js
@@ -11,10 +11,11 @@ self.onmessage = event => {
       pitch: job.pitch,
       grainSize: job.grainSize || 2048,
       hop: job.hop || 512,
+      searchRadius: job.searchRadius,
       onProgress: progress => self.postMessage({ type: 'progress', progress })
     });
     const buffers = result.channels.map(channel => channel.buffer);
-    self.postMessage({type:'done',sampleRate:result.sampleRate,duration:result.duration,length:result.channels[0].length,channels:buffers}, buffers);
+    self.postMessage({type:'done',sampleRate:result.sampleRate,duration:result.duration,length:result.channels[0].length,channels:buffers,stats:{processedGrains:result.processedGrains,totalGrains:result.totalGrains,workRatio:result.workRatio,dirtyRanges:result.dirtyRanges}}, buffers);
   } catch (error) {
     self.postMessage({ type: 'error', message: error?.message || String(error) });
   }
